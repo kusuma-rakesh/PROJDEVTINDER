a28514baeb1dbf54844bec85f9030f60c5a6047d
kusuma-rakesh <116748146+kusuma-rakesh@users.noreply.github.com>
2026-02-24 20:43:23 +0530

    commit feb24

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const app = express();
-const user = {
-  FullName: "Rakesh Kusuma",
-  WorkingAt: "Elp Aviation",
-  EmpCode: "ELP0046",
-};
 
-// NOTE: aapp.use is very danger here -> it always runs event if we try /user as it is matchin /
-// So keep in mind the order., Avoid using app.use()
-app.use("/", (req, res) => {
-  res.send("HAHAHAHAHAHAHHA");
+// app.use("/admin", (req, res) => {
+//   console.log("Printing Admin Filters");
+//   res.send("Admin Filters Page");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Hello Rakesh Kusuma");
+// });
+
+// app.get("/admin/1", (req, res) => {
+//   res.send("i am in admin sub folder");
+// });
+
+// app.get("/abc/:id", (req, res) => {
+//   console.log("print params", req.params);
+//   res.send("Advanced routing with REGEX");
+// });
+//app.use returns if there is a match., it can be usd for GET/POST/DELETE//etc?
+
+//using REGEX
+app.get(/r/, (req, res) => {
+  console.log("Using Regex");
+  res.send("Using Regex");
 });
 
-app.get("/users", (req, res) => {
-  res.send(user);
+app.get(/.*sh$/, (req, res) => {
+  res.send("regex sentence end with the sh particular word");
 });
-app.post("/users", (req, res) => {
-  res.send("POST Method called successfully..!!");
-});
-app.delete("/users", (req, res) => {
-  res.send("DELETE method called successfully..!!");
-});
-app.listen(3700, () => {
-  console.log("applicaiton is running at port >> 3700");
+
+app.listen(7777, () => {
+  console.log("app server started on port 7777");
 });
