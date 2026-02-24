ba8c07b5a3ab062e8be72ca3737eb87e74dace10
kusuma-rakesh <116748146+kusuma-rakesh@users.noreply.github.com>
2026-02-24 21:05:01 +0530

    with req query and params

--- src/app.js
+++ src/app.js
@@ -21,15 +21,42 @@ const app = express();
 //app.use returns if there is a match., it can be usd for GET/POST/DELETE//etc?
 
 //using REGEX
-app.get(/r/, (req, res) => {
-  console.log("Using Regex");
-  res.send("Using Regex");
-});
+// app.get(/r/, (req, res) => {
+//   console.log("Using Regex");
+//   res.send("Using Regex");
+// });
 
 app.get(/.*sh$/, (req, res) => {
   res.send("regex sentence end with the sh particular word");
 });
 
+app.get("/user", (req, res) => {
+  console.log("req params", req.query);
+  res.send("req - query");
+});
+
+app.get("/secure", (req, res) => {
+  console.log("security details", req.query);
+  res.send("res- secutiry");
+});
+
+app.get("/products", (req, res) => {
+  console.log("Product Details: ", req.query);
+  res.send("prod details");
+});
+app.post("/products", (req, res) => {
+  res.send("Post Method Called Successfully...!!!");
+});
+
+app.delete("/products", (req, res) => {
+  res.send("Delete method called successfullt...!");
+});
+
+//Dynamic Routing
+app.get("/emp/:id/:name/:prod", (req, res) => {
+  console.log("emp details", req.params);
+  res.send("emp details with dynamic routing");
+});
 app.listen(7777, () => {
   console.log("app server started on port 7777");
 });
